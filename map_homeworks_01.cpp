5db7537f58c52a05433ad73c0bed7a9f4d7bb4d1
Andrey <andrey.zalozhenkov@pikta.ru>
2023-11-30 15:02:50 +0400

    atomic version

--- map_homeworks_01.cpp
+++ map_homeworks_01.cpp
@@ -1,29 +1,34 @@
 ﻿#include <iostream>
 #include<thread>
 #include<chrono>
+#include<mutex>
+#include<atomic>
 
-int clients{0};
+std::atomic_int clients{0};
+std::mutex m;
 
 void clients_queue(int max_clnts)
 {
+	std::lock_guard lg(m);
 	while (true)
 	{
 		if (clients == max_clnts) { break; }
 		++clients;
 		std::cout << "Queue is " << clients << " clients" << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		//std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
 }
 
 void operationist()
 {
-	std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+	//std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	std::lock_guard lg(m);
 	while (true)
 	{
 		if (!clients) { break; }
 		--clients;
 		std::cout << "Queue is " << clients << " clients" << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(2));
+		//std::this_thread::sleep_for(std::chrono::seconds(2));
 	}
 
 }
@@ -33,13 +38,13 @@ int main()
 	std::thread th1(clients_queue, 20);
 	std::thread th2(operationist);
 
-	th1.detach();
-	th2.detach();
+	th1.join();
+	th2.join();
 
-	std::this_thread::sleep_for(std::chrono::seconds(2));
+	/*std::this_thread::sleep_for(std::chrono::seconds(2));
 	while (true)
 	{
 		if (!clients) { break; }
-	}
+	}*/
 	return 0;
 }
